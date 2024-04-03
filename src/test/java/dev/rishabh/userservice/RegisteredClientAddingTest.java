995fbc4abc53f23e6ee443333ad3143ff1a5a683
rishabhtatia1 <rishabhtatia1@gmail.com>
2024-04-03 11:39:10 +0530

    auth changes

--- src/test/java/dev/rishabh/userservice/RegisteredClientAddingTest.java
+++ src/test/java/dev/rishabh/userservice/RegisteredClientAddingTest.java
@@ -16,28 +16,28 @@ import java.util.UUID;
 
 @SpringBootTest
 public class RegisteredClientAddingTest {
-    @Autowired
-    private RegisteredClientRepository registeredClientRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Test
-    @Commit
-    void addNewRegisteredClient() {
-                RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("productservice")
-                .clientSecret(bCryptPasswordEncoder.encode("passwordofproductserviceclient"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                .redirectUri("https://oauth.pstmn.io/v1/callback")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-
-                registeredClientRepository.save(oidcClient);
-    }
+//    @Autowired
+//    private RegisteredClientRepository registeredClientRepository;
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    @Test
+//    @Commit
+//    void addNewRegisteredClient() {
+//                RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//                .clientId("productservice")
+//                .clientSecret(bCryptPasswordEncoder.encode("passwordofproductserviceclient"))
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+//                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+//                .redirectUri("https://oauth.pstmn.io/v1/callback")
+//                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//
+//                registeredClientRepository.save(oidcClient);
+//    }
 }
